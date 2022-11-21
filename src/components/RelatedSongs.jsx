3dacbcba75fcd1967341a9bd472b3a482aef3a89
kepha222 <kephamotari1@.com>
2022-11-21 16:34:13 +0300

    third commit

--- src/components/RelatedSongs.jsx
+++ src/components/RelatedSongs.jsx
@@ -1,5 +1,29 @@
-const RelatedSongs = () => (
-  <div>Loader</div>
+import SongBar from "./SongBar";
+
+const RelatedSongs = ({
+  data,
+  isPlaying,
+  activeSong,
+  handlePauseClick,
+  handlePlayClick,
+  artistId,
+}) => (
+  <div className="flex flex-col">
+    <h1 className="font-bold text-3xl text-white">Related Songs:</h1>
+    <div className="mt-6 w-full flex flex-col ">
+      {data?.map((song, i) => (
+        <SongBar
+          key={`${song.key}-${artistId}`}
+          song={song}
+          i={i}
+          artistId={artistId}
+          activeSong={activeSong}
+          handlePauseClick={handlePauseClick}
+          handlePlayClick={handlePlayClick}
+        />
+      ))}
+    </div>
+  </div>
 );
 
 export default RelatedSongs;
--- src/redux/services/shazamCore.js
+++ src/redux/services/shazamCore.js
@@ -28,8 +28,18 @@ export const shazamCoreApi = createApi({
   }),
 
   endpoints: (builder) => ({
-    getTopCharts: builder.query({ query: () => "./charts/world" }),
+    getTopCharts: builder.query({ query: () => "/charts/world" }),
+    getSongDetails: builder.query({
+      query: ({ songid }) => `/tracks/details?track_id=${songid}`,
+    }),
+    getSongRelated: builder.query({
+      query: ({ songid }) => `/tracks/related?track_id=${songid}`,
+    }),
   }),
 });
 
-export const { useGetTopChartsQuery } = shazamCoreApi;
+export const {
+  useGetTopChartsQuery,
+  useGetSongDetailsQuery,
+  useGetSongRelatedQuery,
+} = shazamCoreApi;
